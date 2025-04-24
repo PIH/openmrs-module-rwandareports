79a19ce5bffdea8adac2213f12dc38c8307e1741
laureen <laureen.omare@ucgloballprograms.org>
2025-04-24 09:16:10 +0300

    adding payment refunds report

--- omod/src/main/resources/_etl/derived/billing/sp_mamba_data_processing_derived_billing.sql
+++ omod/src/main/resources/_etl/derived/billing/sp_mamba_data_processing_derived_billing.sql
@@ -29,4 +29,5 @@ CALL sp_mamba_fact_patient_service_bill_flat;
 -- Create View
 CALL sp_mamba_view_fact_insurance_report;
 CALL sp_mamba_view_fact_consommation_report;
+CALL sp_mamba_view_fact_paymentrefunds_report;
 -- $END

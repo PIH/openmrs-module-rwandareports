ed7c7c42a4634824dcb83f02e24d7f19db531fbc
laureen <laureen.omare@ucgloballprograms.org>
2025-05-28 09:28:57 +0300

    Add Cashier Report

---
+++ omod/src/main/resources/_etl/derived/billing/facts/fact_cashier_report/sp_mamba_fact_cashier_report_create.sql
@@ -0,0 +1,34 @@
+-- $BEGIN
+
+CREATE TABLE mamba_fact_cashier_report
+(
+    id                      INT             NOT NULL AUTO_INCREMENT,
+    bill_payment_id         INT             NOT NULL,
+    patient_bill_id         INT             NULL,
+    date                    DATETIME        NULL,
+    patient_name            VARCHAR(255)    NULL,
+    amount_paid             DECIMAL(12,2)   NULL,
+    hop_service_id          INT             NULL,
+    global_bill_id          INT             NOT NULL,
+    hop_service_name        VARCHAR(50)     NULL,
+
+    PRIMARY KEY (id)
+)
+    CHARSET = UTF8MB4;
+
+CREATE INDEX mamba_fact_cashier_report_insurance_id_index
+    ON mamba_fact_cashier_report(insurance_id);
+
+CREATE INDEX mamba_fact_cashier_report_global_bill_id_index
+    ON mamba_fact_cashier_reportl (global_bill_id);
+
+CREATE INDEX mamba_fact_cashier_report_hop_service_id_index
+    ON mamba_fact_cashier_report (hop_service_id);
+
+CREATE INDEX mamba_fact_cashier_report_date_index
+    ON mamba_fact_cashier_report (date);
+
+CREATE INDEX mamba_fact_cashier_report_hop_service_name_index
+    ON mamba_fact_cashier_report (hop_service_name);
+
+-- $END
---
+++ omod/src/main/resources/_etl/derived/billing/facts/fact_cashier_report_flat/sp_mamba_fact_cashier_report_flat_create.sql
@@ -0,0 +1,67 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS sp_mamba_fact_cashier_report_flat_create;
+
+CREATE PROCEDURE sp_mamba_fact_cashier_report_flat_create()
+BEGIN
+
+SET session group_concat_max_len = 20000;
+SET @service_columns := NULL;
+
+SELECT GROUP_CONCAT(DISTINCT CONCAT('`', hop_service_id, '` DECIMAL(20, 2)'))
+INTO @service_columns
+FROM mamba_fact_cashier_report;
+
+IF @service_columns IS NULL THEN
+        SET @create_table = CONCAT(
+                'CREATE TABLE mamba_fact_cashier_report_flat (
+                    bill_payment_id         INT             NOT NULL,
+                    patient_bill_id         INT             NULL,
+                    date                    DATETIME        NULL,
+                    patient_name            VARCHAR(255)    NULL,
+                    amount_paid             DECIMAL(12,2)   NULL,
+                    global_bill_id          INT             NOT NULL,
+                    first_date_id           INT             NOT NULL,
+
+                    -- Unique constraints
+                    constraint global_bill_id unique (global_bill_id),
+
+                    -- Indexes
+                    INDEX mamba_fact_cashier_report_flat_global_bill_index (global_bill_id),
+                    INDEX mamba_fact_cashier_report_flat_date_index (date),
+                    INDEX mamba_fact_cashier_report_flat_bill_payment_id_index (bill_payment_id),
+                    INDEX mamba_fact_cashier_report_flat_first_first_date_id_index (first_date_id))'
+            );
+
+ELSE
+        SET @create_table = CONCAT(
+                'CREATE TABLE mamba_fact_cashier_report_flat (
+                    bill_payment_id         INT             NOT NULL,
+                    patient_bill_id         INT             NULL,
+                    date                    DATETIME        NULL,
+                    patient_name            VARCHAR(255)    NULL,
+                    amount_paid             DECIMAL(12,2)   NULL,
+                    global_bill_id          INT             NOT NULL,
+                    first_date_id           INT             NOT NULL,
+                    ', @service_columns, ',
+
+                -- Unique constraints
+                constraint global_bill_id unique (global_bill_id),
+
+                -- Indexes
+                INDEX mamba_fact_cashier_report_flat_global_bill_index (global_bill_id),
+                INDEX mamba_fact_cashier_report_flat_date_index (date),
+                INDEX mamba_fact_cashier_report_flat_bill_payment_id_index (bill_payment_id),
+                INDEX mamba_fact_cashier_report_flat_first_date_id_index (first_date_id))'
+            );
+END IF;
+
+DROP TABLE IF EXISTS `mamba_fact_cashier_report_flat`;
+
+PREPARE createtb FROM @create_table;
+EXECUTE createtb;
+DEALLOCATE PREPARE createtb;
+
+END //
+
+DELIMITER ;
